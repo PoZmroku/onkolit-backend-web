107d975c61abdf2c2659e98ac43e1515ff79780a
PoZmroku <lavlahen9482@gmail.com>
2023-01-13 18:49:13 +0500

    PostController
    - Add multer lib
    - Add getAll, getOne, create, remove and update func for posts
    - Add storage for pic. and app.use for express
    - Create dir. for uploads (for multer)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import multer from 'multer';
 import {registerValidation, loginValidation, postCreateValidation} from './validations/validations.js';
 
 import checkAuth from './utils/checkAuth.js';
@@ -13,13 +14,38 @@ mongoose.connect('mongodb+srv://admin:crosshell4@cluster0.lqzjmlw.mongodb.net/bl
 
 const app = express();
 
-app.use(express.json());
+//хранилище для картинок
+const storage = multer.diskStorage({
+    destination: (_, __, cb ) => {
+        cb(null, 'uploads');
+    },
+    filename: (_, file, cb ) => {
+        cb(null, file.originalname);
+    },
+});
 
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+//роут пользователя
+app.get('/auth/me', checkAuth, UserController.getMe);
 app.post('/auth/login', loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.register);
-app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.get('/posts', checkAuth, postCreateValidation, PostController.create);
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    });
+});
+
+// роут постов
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update);
 
 app.listen(4444, (err) => {
     if(err) {
